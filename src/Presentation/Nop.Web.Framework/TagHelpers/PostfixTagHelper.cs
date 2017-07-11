5cd4beed9ea6d339db490fb527be18a991369d1c
AndreiMaz <andrey.maz@gmail.com>
2017-07-11 22:18:45 +0300

    Added support for "asp-postfix" attribute for <label>. We can use to add colon (:)

---
+++ src/Presentation/Nop.Web.Framework/TagHelpers/PostfixTagHelper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Nop.Web.Framework.TagHelpers
+{
+    [HtmlTargetElement("label", Attributes = PostfixAttributeName)]
+    public class PostfixTagHelper : TagHelper
+    {
+        private const string PostfixAttributeName = "asp-postfix";
+
+        [HtmlAttributeName(PostfixAttributeName)]
+        public string Postfix { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            base.Process(context, output);
+
+            output.Content.Append(Postfix);
+        }
+
+        public override int Order
+        {
+            get
+            {
+                return 100; // Needs to run after aspnet
+            }
+        }
+    }
+}
